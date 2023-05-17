66984ab246fc00f6a96abc5330f8367d97034d2a
Hannele Ruiz <justlemoncl@gmail.com>
2023-05-16 21:45:19 -0400

    Create the path of the game data

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 export XDG_DATA_HOME=/gamedata
+
+if [ ! -d "$XDG_DATA_HOME" ]; then
+  echo "Creating $XDG_DATA_HOME as it does not exists"
+  mkdir $XDG_DATA_HOME
+fi
+
 echo "Starting server at $(date)"
